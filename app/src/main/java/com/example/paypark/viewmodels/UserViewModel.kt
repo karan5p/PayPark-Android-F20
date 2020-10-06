83879864d055041e561c01d1185e7fd308cf2e26
Karan <pate1228@sheridancollege.ca>
2020-10-06 15:08:02 -0400

    Added documentation comment for insertUser() in UserViewModel

---
+++ app/src/main/java/com/example/paypark/viewmodels/UserViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.paypark.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.paypark.database.PayParkDatabase
+import com.example.paypark.database.UserRepository
+import com.example.paypark.model.User
+
+class UserViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val userRepo: UserRepository
+    var allUsers: LiveData<List<User>>
+
+    init {
+        val userDao = PayParkDatabase.getDatabase(application).userDao()
+        userRepo = UserRepository(userDao)
+
+        allUsers = userRepo.allUsers
+    }
+
+    /**
+     * InsertUser(I) method will create a new user record in the database
+     */
+    fun insertAll(){}
+}
